9d39e96006b9360eae05a1972ed8bc22ef2de9c9
dinesh25255 <dineshbhatt0707@gmail.com>
2024-03-19 19:22:43 +0545

    curd operation

---
+++ src/components/array.js
@@ -0,0 +1,32 @@
+// File name - components/array.js
+
+// javascript object named array 
+
+// with 3 key-values
+
+const array =[
+
+    {
+        id: "1",
+        Name : "Ramesh",
+        Age : "23",
+    },
+
+    {
+        id: "2",
+        Name : "Nisha",
+        Age : "24",
+    },
+
+    {
+        id: "3",
+        Name : "Gita",
+        Age : "23",
+    },
+
+   
+
+
+];
+
+export default array;
